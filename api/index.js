3ed2c9759676e954ef93483b1691e92ff1ebb5a1
Dark7cakeforest <160480687+Dark7cakeforest@users.noreply.github.com>
2025-06-18 13:29:03 +0700

    update01

--- api/index.js
+++ api/index.js
@@ -1,23 +1,31 @@
 const http = require('http')
 const fs = require('fs')
 const url = require('url')
-const indexPage = fs.readFileSync('${__dirname}/web/index.html','utf-8')
-const loginPage = fs.readFileSync('${__dirname}/web/login.html','utf-8')
-const staticPage = fs.readFileSync('${__dirname}/web/static.html','utf-8')
-const editPage = fs.readFileSync('${__dirname}/web/edit_plant.html','utf-8')
-const feedbackPage = fs.readFileSync('${__dirname}/web/feedback.html','utf-8')
+const express = require('express')
+const indexPage = fs.readFileSync(`${__dirname}/../web/index.html`,'utf-8')
+const loginPage = fs.readFileSync(`${__dirname}/../web/login.html`,'utf-8')
+const staticPage = fs.readFileSync(`${__dirname}/../web/static.html`,'utf-8')
+const editPage = fs.readFileSync(`${__dirname}/../web/edit_plant.html`,'utf-8')
+const feedbackPage = fs.readFileSync(`${__dirname}/../web/feedback.html`,'utf-8')
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req,res)=>{//สร้าง req,res สำหรับ url ที่เข้าไป
     const {pathname,query} = url.parse(req.url,true)
+    res.writeHead(200)
     if(pathname==="/login" || pathname==="/"){
         res.end(loginPage)
+    }else if(pathname==="/index"){
+        res.end(indexPage)
     }else if(pathname==="/static"){
         res.end(staticPage)
+    }else if(pathname==="/edit_plant"){
+        res.end(editPage)
+    }else if(pathname==="/feedback"){
+        res.end(feedbackPage)
     }else{
         res.writeHead(404)
         res.end("<h1>404 Not found</h1>")
     }
 })
-server.listen(8000,'localhost',()=>{
+server.listen(8000,'localhost',()=>{//เซิฟเวอร์พอร์ต8000
     console.log("Start server at port 8000")
 })
